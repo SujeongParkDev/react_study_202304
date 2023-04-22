f0baab985d845ce2846c2ac87f9fa30ffefc4c04
박수정 <sjj083015@gmail.com>
2023-04-22 23:13:53 +0900

    [ADD] 콜백 중첩 해결 (Promise)

---
+++ projA/src/ch1/readfile-promise.js
@@ -0,0 +1,26 @@
+const fs = require('fs')
+
+// (1) Promise 반환하는 함수 정의
+// Promise 사용하는 경우, Promise 객체 반환하는 함수 선언해야 함.
+function readFile_pr (fname) {
+    return new Promise((resolve) => {
+        fs.readFile(fname, 'utf-8', (err, s) => {
+            resolve(s)
+        })
+    })
+}
+
+// (2) 차례대로 텍스트 파일 읽기
+// 비동기 처리 완료 시 .then() 메서드 내부의 함수가 실행됨.
+readFile_pr('a.txt')
+.then((text) => {
+    console.log('a.txt를 읽어들였습니다.', text)
+    return readFile_pr('b.txt')
+})
+.then((text) => {
+    console.log('b.txt를 읽어들였습니다.', text)
+    return readFile_pr('c.txt')
+})
+.then ((text) => {
+    console.log('c.txt를 읽어들였습니다.', text)
+})
